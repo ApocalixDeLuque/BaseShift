4000b17be4306fbd698e25601025563fd4cad881
Apocalix <lololertrololer@gmail.com>
2023-08-14 15:32:25 -0600

    Initial Commit

---
+++ BaseConverter/BaseConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaseConverterApp.swift
+//  BaseConverter
+//
+//  Created by Jonathan Uriel Perez Vargas on 14/08/23.
+//
+
+import SwiftUI
+
+@main
+struct BaseConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BaseConverter/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  BaseConverter
+//
+//  Created by Jonathan Uriel Perez Vargas on 14/08/23.
+//
+// BaseShift: Numeric Base Converter
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+        .navigationBarTitle("Convert Units")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
